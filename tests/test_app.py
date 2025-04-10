2912fbb1a99639381fba68037f6ebeceea7fd1bd
VIGNESH KUMAR S <vigneshkumarselvkumar@gmail.com>
2025-04-11 01:13:03 +0530

    Create test_app.py

---
+++ tests/test_app.py
@@ -0,0 +1,14 @@
+import json
+import pytest
+from my_application.app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_index(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b'Simple Calculator' in response.data
